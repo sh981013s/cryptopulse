d21762b371873aa032b814db755141bf615c5fc4
sh981013s <sh981013s@gmail.com>
2024-05-15 01:54:30 +0900

    feat: MainPage.tsx to include styled-components and logo image.

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+import logoImg from "../../assets/images/logo.png";
+
+const MainPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 120px);
+
+  h1 {
+    text-align: center;
+  }
+`;
+
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <MainPageWrapper>
+      <div>
+        <img src={logoImg} alt="logo" />
+        <h1>Welcome to CRYPTOPULSE!</h1>
+      </div>
+    </MainPageWrapper>
+  );
 };
 
 export default MainPage;
